5b7f52812a01d9b4b624457eaf0f3a47f4b3d71f
Steven Wilkin <stevenwilkin@gmail.com>
2017-04-19 18:15:52 +0100

    Command parser understands the clear command

--- lib/command_parser.rb
+++ lib/command_parser.rb
@@ -1,5 +1,10 @@
 class CommandParser
   def self.parse(input)
+    case input
+      when 'C'
+        return :clear
+    end
+
     raise StandardError, 'unrecognised command :('
   end
 end
--- spec/lib/command_parser_spec.rb
+++ spec/lib/command_parser_spec.rb
@@ -3,6 +3,8 @@ require 'command_parser'
 RSpec.describe CommandParser do
   describe '.parse' do
     let(:input) { '' }
+    let(:command) { command, *args = parse_input; command }
+    let(:args) { command, *args = parse_input; args }
 
     def parse_input
       described_class.parse(input)
@@ -12,6 +14,13 @@ RSpec.describe CommandParser do
       let(:input) { 'Z 1 1 C' }
 
       specify { expect { parse_input }.to raise_error(/unrecognised/) }
+    end
+
+    context 'with the clear command' do
+      let(:input) { 'C' }
+
+      specify { expect(command).to eq(:clear) }
+      specify { expect(args).to be_empty }
     end
   end
 end
